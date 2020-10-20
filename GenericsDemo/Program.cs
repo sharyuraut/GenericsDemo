81b46527f82ab2ed12544aed2b593b9aad1d284b
sharyu raut <sharyukraut210298@gmail.com>
2020-10-20 09:01:13 +0530

    [Sharyu] Add. Initial commit

---
+++ GenericsDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to generics code.");
+        }
+    }
+}
