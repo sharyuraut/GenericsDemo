575a7d41a9b24721e41bc9eebcc5e6607c1dd92e
sharyu raut <sharyukraut210298@gmail.com>
2020-10-20 09:11:51 +0530

    [Sharyu] Add. printling arrays

--- GenericsDemo/Program.cs
+++ GenericsDemo/Program.cs
@@ -7,6 +7,41 @@ namespace GenericsDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to generics code.");
+
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.1, 2.1, 3.1, 3.4 };
+            char[] charArray = { 'h', 'r', 't' };
+
+            Program.toPrint(intArray);
+            Program.toPrint(doubleArray);
+            Program.toPrint(charArray);
+        }
+
+        private static void toPrint(char[] charArray)
+        {
+            foreach(char element in charArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("---------");
+        }
+
+        private static void toPrint(double[] doubleArray)
+        {
+            foreach (double element in doubleArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("---------");
+        }
+
+        private static void toPrint(int[] intArray)
+        {
+            foreach (int element in intArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("---------");
         }
     }
 }
